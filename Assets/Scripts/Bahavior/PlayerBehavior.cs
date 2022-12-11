0f3e8d56c1a7a7f4673d6c526c18e6c958679afd
Hien Nguyen <85053616+hnq-jan8@users.noreply.github.com>
2022-12-11 23:26:00 +0700

    Code refactor in behavior/jump (#6)
    Dependency injection added

--- Assets/Scripts/Bahavior/PlayerBehavior.cs
+++ Assets/Scripts/Bahavior/PlayerBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PlayerBehavior : MonoBehaviour
-{ 
+{
     public bool UIUsingCheck()
     {
         return UIManager.PlayerisUsingUI;
---
+++ Assets/Scripts/Bahavior/PlayerJumpInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpInput : PlayerBehavior, IJumpingInput
+{
+    public bool trigger { get; private set; }
+
+    public bool release { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TriggerJump();
+    }
+
+    void TriggerJump()
+    {
+        if (UIUsingCheck() == false)
+        {
+            trigger = Input.GetKeyDown(KeyCode.K);
+            release = Input.GetKeyUp(KeyCode.K);
+        }
+        else
+        {
+            trigger = false;
+            release = false;
+        }
+    }
+}
