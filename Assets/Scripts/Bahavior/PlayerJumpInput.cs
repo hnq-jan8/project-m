892d4172ed1b238056153c40bf8586adcb1765e2
Hien Nguyen <85053616+hnq-jan8@users.noreply.github.com>
2022-12-15 20:58:04 +0700

    Jump upgrade
    . Jump height control
    . Double jump
    . Fix groundCheck position

---
+++ Assets/Scripts/Bahavior/PlayerJumpInput.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpInput : PlayerBehavior, IJumpingInput
+{
+    public bool trigger { get; private set; }
+    public bool release { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TriggerJump();
+    }
+
+    void TriggerJump()
+    {
+        if (UIUsingCheck() == false)
+        {
+            trigger = Input.GetKeyDown(KeyCode.K);
+            release = Input.GetKeyUp(KeyCode.K);
+        }
+        else
+        {
+            trigger = false;
+            release = false;
+        }
+    }
+
+    private bool isAirJumped = false;
+    public bool AirJump(bool isGrounded, bool isTrigggered)
+    {
+        //bool canAirJumpLocal = false
+
+        if (isGrounded == true)
+        {
+            isAirJumped = false;
+        }
+        else if (isAirJumped == false && isTrigggered == true)
+        {
+            isAirJumped = true;
+            return true;
+        }
+        return false;
+    }
+}
