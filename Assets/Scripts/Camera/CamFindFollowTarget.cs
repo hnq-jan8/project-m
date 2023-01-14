cd65df8dcf1d17f8411350755ade2234495cd04d
Nguyen Vu Cuong <101236825+Thecoldgod04@users.noreply.github.com>
2023-01-15 00:38:58 +0700

    Checkpoint and shop added, updated Inventory
    Added:
    . Healing system
    . Respawn system
    . Shop system
    . CheckPoint system
    Update:
    . Inventory system

--- Assets/Scripts/Camera/CamFindFollowTarget.cs
+++ Assets/Scripts/Camera/CamFindFollowTarget.cs
@@ -7,10 +7,30 @@ public class CamFindFollowTarget : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera virtualCamera;
     [SerializeField] GameObject targetToFollow;
-    private void Awake()
+    private void Start()
     {
+        FindTargetToFollow();
+    }
+
+    private void OnEnable()
+    {
+        //FindTargetToFollow();
+    }
+
+    public void FindTargetToFollow()
+    {
+        //Debug.Log("abc");
+
         targetToFollow = FindObjectOfType<PlayerSingleton>().gameObject;
 
         virtualCamera.Follow = targetToFollow.transform;
+        //Debug.Log(virtualCamera.Follow == targetToFollow.transform);
     }
+
+   /* private void Update()
+    {
+        targetToFollow = FindObjectOfType<PlayerSingleton>().gameObject;
+        virtualCamera.Follow = targetToFollow.transform;
+        Debug.Log(targetToFollow.name);
+    }*/
 }
