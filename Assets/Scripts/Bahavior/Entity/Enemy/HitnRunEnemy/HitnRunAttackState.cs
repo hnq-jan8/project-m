5651ef17eccab854c60e91da2d515a9d2eb12413
Thecoldgod04 <thecoldsea0412@gmail.com>
2023-02-10 21:43:19 +0700

    Finished hit-n-run creep with animation

--- Assets/Scripts/Bahavior/Entity/Enemy/HitnRunEnemy/HitnRunAttackState.cs
+++ Assets/Scripts/Bahavior/Entity/Enemy/HitnRunEnemy/HitnRunAttackState.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HitnRunAttackState : IHitnRunState
 {
     //this is the duration of the attack animation
-    private float timeInState = 1f;
+    private float timeInState = 0.75f;
     public IHitnRunState DoState(HitnRunStateMachine hitnRunStateMachine)
     {
         //play attack animation
@@ -13,9 +13,11 @@ public class HitnRunAttackState : IHitnRunState
         if(timeInState <= 0)
         {
             //reset time in state
-            timeInState = 1f;
+            timeInState = 0.75f;
+            hitnRunStateMachine.movementData.anim.SetBool("Attack", false);
             return hitnRunStateMachine.retreatState;
         }
+        hitnRunStateMachine.movementData.anim.SetBool("Attack", true);
         return hitnRunStateMachine.attackState;
     }
 }
